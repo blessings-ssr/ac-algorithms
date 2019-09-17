65ab1da5106b0b00cbcda94fca63f5c5c3d4be83
blessings-ssr <blessings@anniecannons.com>
2019-09-17 10:52:35 -0700

    solution for factorializeit exercise

--- factorializeIt.js
+++ factorializeIt.js
@@ -7,8 +7,20 @@
 
 // Check out Arithmetic Operators https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
-function factorializeIt(num) {
-  //your code here
-}
+//Input a whole number, Integer
 
-factorializeIt(5);
+//mean the product of all of the numbers between 1 and that number
+// Use a variable to store the output (the product)
+// Multiplication operator (*)
+// Is there a JavaScript method that calculates a factorial?
+// Loop: we need to start at 1 and end at the number that is the parameter, and instead of adding each time in the loop, we can multiply
+
+ function factorialize(num) {
+   var output = 1;
+   for (var i = 1; i <= num; i++){
+     output = output * i; 
+   } 
+   return output;
+ } 
+ var result = factorialize(5);
+ consule.log(result); 
