3113d4ebe9137424f2db642b7e3b921546f9b2a8
blessings-ssr <blessings@anniecannons.com>
2019-09-18 11:11:50 -0700

    solution to longestWord

--- longestWord.js
+++ longestWord.js
@@ -18,8 +18,22 @@
 // findLongestWord("What is the average airspeed velocity of an unladen swallow") should return 8.
 // findLongestWord("What if we try a super-long word such as otorhinolaryngology") should return 19.
 
-
+//input : a string
+//output : a number
+//we need to look at the length of each word, we need to compare the length of each word with the word with the others in the string. If a word is longer then the one that comes before it, we wll replace th evalue of its length.
+// Tools: a variable to store the length of the longest string
+//.split to chage our string and turns it into an array
+//for loop : run through the words
+//condtional statement: checkto see if the next word is longer than the word that came before it. if true I want to replace the value of the varaible with the lienght o fthe longer word.
 function findLongestWord(str) {
-  //your code here
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
 }
-
+console.log(findLongestWord("Happy Birthday to you"));
+//8
