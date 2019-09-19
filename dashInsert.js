dd0c30addc141d04d9953663cc58e56a73435dcb
blessings-ssr <blessings@anniecannons.com>
2019-09-19 11:33:36 -0700

    completed solution to dashinsert

--- dashInsert.js
+++ dashInsert.js
@@ -25,6 +25,28 @@ console.log(result) // "1-3"
 
 **/
 
-function dashInsert(str) {
-	// Your code
+//Input: a number
+//Output: a string, with dashes between the odd numbers.
+
+//Create a variable to store the output string
+//Create a variable to store the transformation of the number first to a string and them to an array tools: .toString().split("")
+4982
+["4", "9", "8", "2"]
+//Create a for loop to go thru each of the numbers in the array 
+	// Need a conditional statement that will test the number in the loop is odd && if the number after it is odd.
+		//If true, I want to add the number and the dash to the string
+		//If false I want to just add the number
+
+function dashInsert(num) {
+	var output = "";
+	var array = num.toString().split("");
+	for (var i = 0; i < array.length; i++){
+		if( array[i] % 2 === 1 && array[i + 1] % 2 === 1)
+		{
+			output = output + array[i] + "-";
+		} else {
+			output = output + array[i];
+		}
+	}
 }
+dashInsert(34567);
